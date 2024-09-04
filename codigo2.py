5413c35cff3585f4071f3dbcf27dacb5e877c7c0
0bam4-rf <fquintero164@gmail.com>
2024-09-03 20:54:39 -0500

    3 actulizacion

--- codigo2.py
+++ codigo2.py
@@ -1,5 +1,24 @@
 #------reservaciones-------
-reservaciones = {"1reservacion"["lugar":"san andres","cantidad": 5 ,"transporte": "avion","tiempo": "7 dias"],
-                 "2reservacion"["lugar":"cartagena","cantidad": 4 , "transporte": "autobus","tiempo": "5 dias"]
+reservaciones = {"1reservacion"{"lugar":"san andres","cantidad": 5 ,"transporte": "avion","tiempo": "7 dias"},
+                 "2reservacion"{"lugar":"cartagena","cantidad": 4 , "transporte": "autobus","tiempo": "5 dias"}
                  }
 
+
+print(reservaciones)
+print("QUE ACCION DESEA REALIZAR?")
+print("MODIFICAR RESERVACION (1)")
+print("CONFIRMAR RESERVACION (2)")
+print("ELIMINAR RESERVACION (3)")
+respuesta = input ("ESCRIBA UNA OPCION: ")
+while True:
+    if respuesta == "1":
+        print("MODIFICAR RESERVACION")
+        print(reservaciones)
+        opcion = print("SELECIONE LA RESERVACION A MODIFIRCAR: ")
+
+    elif respuesta == "2":
+        print("CPNFIRMAR RESERVACION") 
+    elif respuesta == "3":
+        print("ELIMINAR RESERVACION") 
+    else:
+        print("INGRESE UNA OPCION VALIDAD")        
