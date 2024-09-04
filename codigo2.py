370ee64ab055187a31ba09b0f78818a3ab02ab76
0bam4-rf <fquintero164@gmail.com>
2024-09-03 19:41:00 -0500

    1actilizacion

---
+++ codigo2.py
@@ -0,0 +1,5 @@
+#------reservaciones-------
+reservaciones = {"1reservacion"["lugar":"san andres","cantidad": 5 ,"transporte": "avion","tiempo": "7 dias"]
+                 "2reservacion"[]
+                 }
+
